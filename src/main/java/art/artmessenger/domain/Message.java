53547ea9d61e83d13e83e4f63ea2cd132a36f8c0
ArtyemTS <tsibenkov.artyem@gmail.com>
2019-01-31 12:44:31 +0300

    Done

--- src/main/java/art/artmessenger/domain/Message.java
+++ src/main/java/art/artmessenger/domain/Message.java
@@ -1,10 +1,12 @@
 package art.artmessenger.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonView;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table
@@ -12,8 +14,18 @@ import javax.persistence.Table;
 @EqualsAndHashCode(of = {"id"})
 public class Message {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonView(Views.Id.class)  //Скрываем поля. Все задается в классе Views
     private Long id;
+
+    @JsonView(Views.IdName.class)
     private String text;
+
+    @Column(updatable = false) // Делаем поле необновляемым
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss") //Установили формат даты
+    @JsonView(Views.FullMessage.class)
+    private LocalDateTime creationDate;
 
     public Long getId() {
         return id;
@@ -29,5 +41,13 @@ public class Message {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
     }
 }
