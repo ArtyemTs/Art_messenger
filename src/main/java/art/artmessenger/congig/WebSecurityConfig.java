24aa9cc7eb6f7ebb33b462827103b624fc698846
ArtyemTS <tsibenkov.artyem@gmail.com>
2019-02-01 17:44:36 +0300

    Посмотреть запись в базе postgres=# \c aaa Вы подключены к базе данных "aaa" как пользователь "postgres". aaa=# select * from usr;

--- src/main/java/art/artmessenger/congig/WebSecurityConfig.java
+++ src/main/java/art/artmessenger/congig/WebSecurityConfig.java
@@ -4,16 +4,18 @@ import art.artmessenger.domain.User;
 import art.artmessenger.repo.UserDetailsRepo;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import java.time.LocalDateTime;
+
 @Configuration
 @EnableWebSecurity
 @EnableOAuth2Sso
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -27,11 +29,31 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     // Созранение авторизованного пользователя в БД
     // Фабричный метод:
-    public PrincipalExtractor principalExtractor(UserDetailsRepo userDetailsRepo){
-
+    @Bean
+    public PrincipalExtractor principalExtractor(UserDetailsRepo userDetailsRepo) {
         return map -> {
-            return new User();  //Возвращает заглушку в виде нового пользователя
+            String id = (String) map.get("sub");
+
+            User user = userDetailsRepo.findById(id).orElseGet(() -> {
+                User newUser = new User();
+
+                newUser.setId(id);
+                newUser.setName((String) map.get("name"));
+                newUser.setEmail((String) map.get("email"));
+                newUser.setGender((String) map.get("gender"));
+                newUser.setLocale((String) map.get("locale"));
+                newUser.setUserpic((String) map.get("picture"));
+
+                return newUser;
+            });
+
+            user.setLastVisit(LocalDateTime.now());
+
+            return userDetailsRepo.save(user);
         };
     }
+//        return map -> {
+//            return new User();  //Возвращает заглушку в виде нового пользователя
+//        };
 
 }
