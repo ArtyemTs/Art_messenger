a5ccbb81acea7f319c8f77ebf918fae952a369c9
ArtyemTS <tsibenkov.artyem@gmail.com>
2019-02-02 16:23:31 +0300

    Добавлен session_tables.sql скрипт

--- src/main/java/art/artmessenger/domain/User.java
+++ src/main/java/art/artmessenger/domain/User.java
@@ -7,13 +7,14 @@ import lombok.Data;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 
 @Entity
 @Table(name = "usr")
 @Data
-public class User {
+public class User implements Serializable {
     @Id
     // Данные которые будут приходить с Гугла
     private String id;
